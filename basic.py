70b7d5559186c2431803dc606f919dbbc2464639
Deeksha <deekshak2103@gmail.com>
2025-04-12 17:56:33 +0530

    week2

--- basic.py
+++ basic.py
@@ -1,7 +1,20 @@
 import cv2 as cv
-img = cv.imread("../images/girl.jpeg")
-cv.imshow("image",img)
-imgresi = cv.resize(img,(200,599))
-cv.imshow("resized",imgresi)
-cv.waitKey(0)
+# img = cv.imread("../images/girl.jpeg")
+# cv.imshow("image",img)
+# imgresi = cv.resize(img,(200,599))
+# cv.imshow("resized",imgresi)
+capture = cv.VideoCapture(0)
+while True:
+    isTrue, frame = capture.read()
+    if not isTrue:
+        break
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    cv.imshow('frame',frame)
+    cv.imshow("grayframe",frame_gray)
+    cv.imshow("rgbframe",frame_rgb)
+    if cv.waitKey(3) & 0xFF==ord('d'):
+        break
+capture.release()
 cv.destroyAllWindows()
+
